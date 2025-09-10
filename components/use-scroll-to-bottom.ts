d0ffc0657a2a9b9a7da95c6b9b5ccbc601837735
l3en00725 <ben@buddsplumbing.com>
2025-09-09 21:57:14 -0400

    Update use-scroll-to-bottom.ts

--- components/use-scroll-to-bottom.ts
+++ components/use-scroll-to-bottom.ts
@@ -1,3 +1,25 @@
+"use client";
+
+import { useEffect, useRef, type RefObject } from "react";
+
+export function useScrollToBottom<T extends HTMLElement>(): [
+  RefObject<T>,
+  () => void
+] {
+  const ref = useRef<T>(null);
+
+  const scrollToBottom = () => {
+    if (ref.current) {
+      ref.current.scrollTop = ref.current.scrollHeight;
+    }
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, []);
+
+  return [ref, scrollToBottom];
+}
 import { useEffect, useRef, type RefObject } from 'react';
 
 export function useScrollToBottom<T extends HTMLElement>(): [
